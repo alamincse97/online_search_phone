45965aacc8fc33530ee16391190c4a43307bab1f
ImAlAmin <alamin.cse97@gmail.com>
2022-12-16 10:02:07 +0600

    added to fetch api data

---
+++ js/api.js
@@ -0,0 +1,5 @@
+/* Fetch API data from server after clicking the "Search" button */
+const btnClicked = () =>{
+    const searchField = document.getElementById('search-field');
+    const searchFieldText = searchField.value;
+    console.log(searchFieldText);
